ff42ab0ec9d621645fe8be49058140e3d840ca38
Jiale Zhi <vipcalio@gmail.com>
2014-02-28 16:19:27 -0800

    Some documentation work.

--- capnp.lua
+++ capnp.lua
@@ -1,3 +1,8 @@
+-----------------------------------------------------------
+-- lua-capnproto runtime module.
+-- @copyright 2013-2014 Jiale Zhi (vipcalio@gmail.com)
+-----------------------------------------------------------
+
 local ffi = require "ffi"
 local bit = require "bit"
 
@@ -18,6 +23,7 @@ local byte      = string.byte
 local type      = type
 local modf      = math.modf
 local substr    = string.sub
+local error     = error
 
 -- Only works with Little Endian for now
 assert(ffi.abi("le") == true)
@@ -45,14 +51,6 @@ end
 local _M = new_tab(0, 32)
 
 
-local function get_bit_offset(bit_off, size)
-    local n, s
-    n = floor(bit_off / size)
-    s = bit_off % size
-
-    return n, s
-end
-
 local pint8    = typeof("int8_t *")
 local pint16   = typeof("int16_t *")
 local pint32   = typeof("int32_t *")
@@ -79,6 +77,23 @@ local pointer_map = {
     float64 = typeof("double *"),
 }
 
+--- Calculate offset within size
+-- @param bit_off       offset in bits
+-- @param size          size in bits
+-- @return              n: number of size-long space
+-- @return              s: offset within a size-long space
+local function get_bit_offset(bit_off, size)
+    local n, s
+    n = floor(bit_off / size)
+    s = bit_off % size
+
+    return n, s
+end
+
+--- Get a pointer cdata from a int32 pointer and a Cap'n Proto type
+-- @param p32           int32 pointer
+-- @param field_type    Cap'n Proto type string
+-- @return              pointer cdata
 local function get_pointer_from_type(p32, field_type)
     local t = pointer_map[field_type]
     if not t then
@@ -87,67 +102,48 @@ local function get_pointer_from_type(p32, field_type)
 
     return cast(t, p32)
 end
---[[
-local function get_pointer_from_val(p32, size, val)
-    local buf = p32
-    local p = buf
-    if size == 1 then
-        p = cast(puint8, buf)
-    else
-        local i, f = modf(val)
-        -- float number
-        if (f ~= 0) then
-            if size == 32 then
-                p = cast(pfloat32, p)
-            elseif size == 64 then
-                p = cast(pfloat64, p)
-            else
-                error("float size other than 32 and 64")
-            end
-        else
-            if size == 64 then
-                p = cast(puint64, buf)
-            elseif size == 32 then
-                p = cast(puint32, buf)
-            elseif size == 16 then
-                p = cast(puint16, buf)
-            elseif size == 8 then
-                p = cast(puint8, buf)
-            else
-                error("unknown in size " .. size)
-            end
-        end
-    end
-    return p
-end
-]]
+
+--- Use LuaJIT FFI to calculate xor for float number
+-- @param val       input float32 number
+-- @param default   its default value
+-- @return          xor'ed value
 function _M.fix_float32_default(val, default)
     local uint, float
     bfloat32[0] = default
     bfloat32[1] = val
-    uint_def = cast(puint32, bfloat32)
-    uint_val = cast(puint32, bfloat32 + 1)
-    --print("before float32_def", uint_val[0], uint_def[0])
+
+    -- default float value
+    local uint_def = cast(puint32, bfloat32)
+    local uint_val = cast(puint32, bfloat32 + 1)
     val = bxor(uint_val[0], uint_def[0])
-    --print("after float32_def", uint_val[0], uint_def[0])
     bint32[0] = val
     float = cast(pfloat32, bint32)
     return float[0]
 end
 
+--- Use LuaJIT FFI to calculate xor for float number
+-- @param val       input float64 number
+-- @param default   its default value
+-- @return          xor'ed value
 function _M.fix_float64_default(val, default)
     local uint, float
     bfloat64[0] = default
     bfloat64[1] = val
-    uint_def = cast(puint64, bfloat64)
-    uint_val = cast(puint64, bfloat64 + 1)
+    local uint_def = cast(puint64, bfloat64)
+    local uint_val = cast(puint64, bfloat64 + 1)
     val = bxor(uint_val[0], uint_def[0])
     buint64[0] = val
     float = cast(pfloat64, buint64)
     return float[0]
 end
 
--- default: optional
+--- Read a structure field
+-- @param p32           a int32 pointer points to the start of a struct
+-- @param field_type    struct field type
+-- @param size          field size in bits
+-- @param off           field offset (from parsed schema)
+-- @param default       field default value
+-- @return              parsed field
 function _M.read_struct_field(p32, field_type, size, off, default)
     local buf = p32
     if field_type == "void" then
@@ -198,6 +194,7 @@ function _M.read_struct_field(p32, field_type, size, off, default)
     end
 end
 
+--- Write a bit (boolean value)
 function _M.write_bit(p8, val, bit_off, default)
     if type(val) == "boolean" then
         val = val and 1 or 0
@@ -208,6 +205,7 @@ function _M.write_bit(p8, val, bit_off, default)
     p8[0] = bor(p8[0], lshift(val, bit_off))
 end
 
+--- Write a number
 function _M.write_num(p, val, field_type, default)
     if default then
         if field_type == "float32" then
@@ -221,7 +219,13 @@ function _M.write_num(p, val, field_type, default)
     p[0] = val
 end
 
--- default: optional
+--- Write a structure field
+-- @param p32           a int32 pointer points to the start of a struct
+-- @param val           field value
+-- @param field_type    struct field type
+-- @param size          field size in bits
+-- @param off           field offset (from parsed schema)
+-- @param default       field default value
 function _M.write_struct_field(p32, val, field_type, size, off, default)
     --local p = get_pointer_from_val(p32, size, val)
     local p = get_pointer_from_type(p32, field_type)
@@ -240,10 +244,16 @@ function _M.write_struct_field(p32, val, field_type, size, off, default)
     end
 end
 
+--- Read text (not including list pointer) starting from buf, works for both
+-- "text" type and "data" type
 function _M.read_text_data(buf, num)
     return ffistr(buf, num) -- dataWordCount + offset + pointerSize + data_off
 end
 
+--- Write text (not including list pointer) starting from buf
+-- @param buf       free space that text data will be written to
+-- @text            text to be written
+-- @is_binary       if writes "data", is_binary is true, otherwise false.
 function _M.write_text_data(buf, text, is_binary)
     local len = #text
     copy(buf, text, len)
@@ -258,17 +268,19 @@ function _M.read_text(buf, header, T, offset, default)
     local res
     local data_off, size, num = _M.read_listp_struct(buf, header, T, 2)
     if data_off and num then
-        res = ffistr(buf + (T.dataWordCount + offset + 1 + data_off) * 2, num - 1) -- dataWordCount + offset + pointerSize + data_off
+        res = ffistr(buf + (T.dataWordCount + offset + 1 + data_off) * 2,
+                num - 1) -- dataWordCount + offset + pointerSize + data_off
     else
         res = default
     end
     return res
 end
 
--- write text
--- @p32
--- @text
--- @data_off            words between the end of list pointer and the first byte of text data
+--- Write text
+-- @param p32
+-- @param text
+-- @param data_off          words between the end of list pointer and the first
+--                          byte of text data
 function _M.write_text(p32, text, data_off, is_binary)
     local len = #text
     if not is_binary then
@@ -285,14 +297,16 @@ function _M.get_data_off(T, offset, pos)
 end
 
 function _M.read_composite_tag(buf)
-    local p = ffi.cast("int32_t *", buf)
+    local p = cast(pint32, buf)
     local val = p[0]
     local sig = band(val, 0x03)
     if sig ~= 0 then
-        error("corrupt data, expected struct signiture(composite list tag) 0 but have " .. sig)
+        error("corrupt data, expected struct signiture(composite list tag) " ..
+                "0 but have " .. sig)
     end
 
-    local num = rshift(val, 2)   -- pointer offset (B) instead indicates the number of elements in the list
+    -- pointer offset (B) instead indicates the number of elements in the list
+    local num = rshift(val, 2)
     val = p[1]
     local dt = band(val, 0xffff)
     local pt = rshift(val, 16)
@@ -303,11 +317,12 @@ end
 function _M.write_composite_tag(p32, T, num)
     --local p = ffi.cast("int32_t *", buf)
     local p = p32
-    p[0] = lshift(num, 2)   -- pointer offset (B) instead indicates the number of elements in the list
+    -- pointer offset (B) instead indicates the number of elements in the list
+    p[0] = lshift(num, 2)
     p[1] = lshift(T.pointerCount, 16) + T.dataWordCount
 end
 
-function read_struct_pointer(p)
+function _M.read_struct_pointer(p)
     local offset = rshift(p[0], 2)
     local data_word_count = band(p[1], 0xffff)
     local pointer_count = rshift(p[1], 16)
@@ -367,17 +382,18 @@ function _M.read_listp(p32, header)
         -- return read_list_pointer(p32)
     elseif sig == 2 then
         --print("single far pointer")
-        return read_far_pointer(p32, header, _M.read_listp)
+        return _M.read_far_pointer(p32, header, _M.read_listp)
     else
-        error("corrupt data, expected list signiture 1 or far pointer 2, but have " .. sig)
+        error("corrupt data, expected list signiture 1 or far pointer 2, " ..
+                "but have " .. sig)
     end
 end
 
 -- write list pointer to a pointed memory
--- @p32         32 bit pointer
--- @size_type   element size type
--- @num         number of elements
--- @data_off    data offset of this list pointer
+-- @param p32         32 bit pointer
+-- @param size_type   element size type
+-- @param num         number of elements
+-- @param data_off    data offset of this list pointer
 function _M.write_listp(p32, size_type, num, data_off)
     p32[0] = lshift(data_off, 2) + 1
     p32[1] = lshift(num, 3) + size_type
@@ -391,7 +407,8 @@ function _M.write_listp_buf(p32, T, offset, size_type, num, data_off)
     p32[base + 1] = lshift(num, 3) + size_type
 end
 
--- see http://kentonv.github.io/_Mroto/encoding.html#lists
+--- map size type to its size
+-- @see http://kentonv.github.io/_Mroto/encoding.html#lists
 local list_size_map = {
     [0] = 0,
     [1] = 0.125,
@@ -421,8 +438,9 @@ local type_to_size_type = {
     struct  = 7, -- composite
 }
 
--- @p32     write list data to this position
--- @return space consumed in bytes
+--- Write list data (not including list pointer)
+-- @param p32       write list data to this position
+-- @return          space consumed in bytes
 -- space is allocated by itself
 function _M.write_list_data(p32, data, pos, elm_type, ...)
     local start = pos
@@ -433,30 +451,16 @@ function _M.write_list_data(p32, data, pos, elm_type, ...)
     if elm_type == "list" then
         pos = pos + len * 8
         for i = 1, len do
-            pos = pos + _M.write_list(p32 + (i - 1) * 2, data[i], pos - 8 * (i - 1), elm_type, ...)
-            --[[
-            local data_off = (pos - i * 8) / 8 -- pos is in bytes
-            local child_num = #data[i]
-            local size_type = type_to_size_type[elm_type]
-            if not size_type then
-                error("unsupported elm_type: " .. elm_type)
-            end
-            _M.write_listp(p32 + (i - 1) * 2, size_type, child_num, data_off)
-            pos = pos + _M.write_list_data(p32 + pos / 4, data[i], ...)
-            ]]
+            pos = pos + _M.write_list(p32 + (i - 1) * 2, data[i], pos -
+                    8 * (i - 1), elm_type, ...)
         end
     elseif elm_type == "text" then
         pos = pos + 8 * len
         for i = 1, len do
 
             local data_off = (pos - i * 8) / 8 -- pos is in bytes
-            pos = pos + _M.write_text(p32 + (i - 1) * 2, data[i], data_off, false)
---[[
-            local data_off = (pos - i * 8) / 8 -- pos is in bytes
-            local str_len = #data[i] + 1
-            _M.write_listp(p32 + (i - 1) * 2, 2, str_len, data_off)
-            pos = pos + _M.write_text_data(p32 + pos / 4, data[i], false)
-            ]]
+            pos = pos + _M.write_text(p32 + (i - 1) * 2, data[i], data_off,
+                    false)
         end
     elseif elm_type == "data" then
         pos = pos + 8 * len
@@ -473,17 +477,15 @@ function _M.write_list_data(p32, data, pos, elm_type, ...)
         pos = pos + 8
         local offset = pos
 
-        --local sp32 = p32 + pos / 4
         local struct_size = (T.dataWordCount + T.pointerCount) * 8
         pos = pos + struct_size * len
         for i = 1, len do
-             sp32 = p32 + offset / 4
+             local sp32 = p32 + offset / 4
              local new_pos = pos - offset-- - offset
 
              local ssize = T.flat_serialize(data[i], sp32, new_pos)
 
              pos = pos + ssize - struct_size
-             --pos = pos + T.flat_serialize(data[i], sp32, pos - offset)
              offset = offset + struct_size
         end
     elseif elm_type == "bool" then
@@ -496,7 +498,8 @@ function _M.write_list_data(p32, data, pos, elm_type, ...)
     else
         local p = get_pointer_from_type(p32, elm_type)
         for i = 1, len do
-            -- No default value avaliable from AST, so no need to pass default value
+            -- No default value avaliable from AST, so no need to pass
+            -- default value
             _M.write_num(p + i - 1, data[i], elm_type)
         end
         local size = assert(list_size_map[type_to_size_type[elm_type]])
@@ -505,9 +508,10 @@ function _M.write_list_data(p32, data, pos, elm_type, ...)
     return pos - start
 end
 
--- @p32
--- @pos     free space offset from p32
+--- Write a list (including list pointer and list data)
 -- space for list pointer is allocated from outside
+-- @param p32
+-- @param pos     free space offset from p32
 function _M.write_list(p32, data, pos, typ, ...)
     local size = 0
     local data_off = (pos - 8) / 8 --get_data_off(parentT, offset, pos)
@@ -531,16 +535,16 @@ function _M.write_list(p32, data, pos, typ, ...)
         num = (T.dataWordCount + T.pointerCount) * num
     end
     --write_listp_buf(p32, parentT, offset, size_type, len, data_off)
-    --print("write listp", "size", size_type, "num", num, "off", data_off, "pos", pos)
     _M.write_listp(p32, size_type, num, data_off)
     --print("write list done")
     return size
 end
--- read data part of a list
--- @p           start of data buffer
--- @header      stream header, see http://kentonv.github.io/capnproto/encoding.html#serialization_over_a_stream
--- @num         number of elements in this list
--- @elm_type    elememt type: "int32", "data", "list", etc.
+
+--- read data part of a list
+-- @param p           start of data buffer
+-- @param header      stream header, see http://kentonv.github.io/capnproto/encoding.html#serialization_over_a_stream
+-- @param num         number of elements in this list
+-- @param elm_type    elememt type: "int32", "data", "list", etc.
 function _M.read_list_data(p32, header, num, elm_type, ...)
     p32 = cast(puint32, p32)
     if not elm_type then
@@ -552,16 +556,19 @@ function _M.read_list_data(p32, header, num, elm_type, ...)
     if elm_type == "list" then
         -- print("list data: list")
         for i = 1, num do
-            local off, child_size, child_num = _M.read_listp_list(p32, header, i)
+            local off, child_size, child_num = _M.read_listp_list(p32,
+                    header, i)
             if off and child_num then
-                t[i] = _M.read_list_data(p32 + (i + off) * 2, header, child_num, ...)
+                t[i] = _M.read_list_data(p32 + (i + off) * 2, header,
+                        child_num, ...)
             end
         end
 
     elseif elm_type == "text" then
         -- print("list data: text: ", num)
         for i = 1, num do
-            local off, child_size, child_num = _M.read_listp_list(p32, header, i)
+            local off, child_size, child_num = _M.read_listp_list(p32,
+                    header, i)
             --print(off, child_size, child_num)
             if off and child_num then
                 t[i] = _M.read_text_data(p32 + (i + off) * 2, child_num - 1)
@@ -570,7 +577,8 @@ function _M.read_list_data(p32, header, num, elm_type, ...)
     elseif elm_type == "data" then
         -- print("list data: data")
         for i = 1, num do
-            local off, child_size, child_num = _M.read_listp_list(p32, header, i)
+            local off, child_size, child_num = _M.read_listp_list(p32,
+                    header, i)
             if off and child_num then
                 t[i] = _M.read_text_data(p32 + (1 + off) * 2, child_num)
             end
@@ -585,7 +593,8 @@ function _M.read_list_data(p32, header, num, elm_type, ...)
         for i = 1, real_num do
             -- TODO reuse table
             t[i] = new_tab(0, 8)
-            T.parse_struct_data(p32 + 2 + (i - 1) * struct_size / 4, dt, pt, header, assert(t[i]))
+            T.parse_struct_data(p32 + 2 + (i - 1) * struct_size / 4, dt,
+                    pt, header, assert(t[i]))
         end
     else
         --[[
@@ -606,20 +615,6 @@ function _M.read_list_data(p32, header, num, elm_type, ...)
     return t
 end
 
---[[
-function read_list_pointer(buf)
-    local p = buf
-    local val = p[0]
-    local offset = rshift(val, 2)
-
-    val = p[1]
-    local size_type = band(val, 0x07)
-    local num = rshift(val, 3)
-
-    return offset, size_type, num
-end
---]]
-
 -- @index: start from 1
 function _M.read_listp_list(p32, header, index)
     return _M.read_listp(p32 + (index - 1) * 2, header)
@@ -629,20 +624,10 @@ function _M.read_listp_struct(buf, header, T, offset)
     local p = cast(pint32, buf)
     local base = T.dataWordCount * 2 + offset * 2
 
---    local sig = band(val, 0x03)
---    if sig == 1 then
-        --print("plain pointer")
-        return _M.read_listp(p + base, header)
---    elseif sig == 2 then
-        --print("single far pointer")
---        return read_far_pointer(p + base, header, read_list_pointer)
---    else
---        error("corrupt data, expected list signiture 1 or far pointer 2, but have " .. sig)
---    end
-
+    return _M.read_listp(p + base, header)
 end
 
-function read_far_pointer(buf, header, parser)
+function _M.read_far_pointer(buf, header, parser)
     local p = buf
 
     local landing = rshift(band(p[0], 0x04), 2)
@@ -658,11 +643,9 @@ function read_far_pointer(buf, header, parser)
         op_offset = op_offset + header.seg_sizes[i]
     end
     op_offset = op_offset + offset  -- offset is in words
-    --print("op_offset:", op_offset)
     local pp = header.base + op_offset * 2 -- header.base is uint32_t *
 
     local p_offset, r1, r2 = parser(pp, header)
-    --print("p_offset:", p_offset)
     p_offset = p_offset + (pp - p) / 2 -- p and pp are uint32_t *
 
     return p_offset, r1, r2
@@ -678,11 +661,12 @@ function _M.read_struct_buf(p32, header)
     local sig = band(p[0], 0x03)
 
     if sig == 0 then
-        return read_struct_pointer(p)
+        return _M.read_struct_pointer(p)
     elseif sig == 2 then
-        return read_far_pointer(p, header, read_struct_pointer)
+        return _M.read_far_pointer(p, header, _M.read_struct_pointer)
     else
-        error("corrupt data, expected struct signiture 0 or far pointer 2, but have " .. sig)
+        error("corrupt data, expected struct signiture 0 or far pointer 2, " ..
+                "but have " .. sig)
     end
 end
 
